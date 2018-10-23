569b92e0a9b25d34b48fd449a00bfe6eabc2e5b2
shiyajian <shiyajian1991@gmail.com>
2018-10-23 22:32:31 +0800

    计划打造和支付相关的模块，增加了一个支付模块，实现了eureka-client

---
+++ cloud-global/src/main/java/com/shiyajian/cloud/global/utils/NetUtils.java
@@ -0,0 +1,53 @@
+package com.shiyajian.cloud.global.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author shiyajian
+ * create: 2018-10-23
+ */
+public final class NetUtils {
+
+    private static final String UNKNOWN = "unknown";
+    /**
+     * 获得请求的真实ip
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0
+                    || UNKNOWN.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0
+                    || UNKNOWN.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0
+                    || UNKNOWN.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0
+                    || UNKNOWN.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0
+                    || UNKNOWN.equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+        } else if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (String ipStr : ips) {
+                if (!(UNKNOWN.equalsIgnoreCase(ipStr))) {
+                    return ipStr;
+                }
+            }
+        }
+        return ip;
+    }
+}
